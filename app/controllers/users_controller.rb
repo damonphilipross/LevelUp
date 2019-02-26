ac42a3559434356a56f08b52c44cf42d8aeff7ee
dale hindle <dale.hindle@yahoo.ca>
2019-02-26 16:15:23 +1100

    made changes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def profile
+    @user = current_user
+    if current_user.influencer
+      # challenges created by influencer
+      @challenges = current_user.challenges
+    else
+      @challenges = current_user.challenge_participations
+      # challenges this user is participating in
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    has_many :challenges
+    has_many :participants
+    has_many :challenge_participations, through: :participants, source: :challenges
 end
