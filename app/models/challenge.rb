706d3789e6fa08898e3527a21837bb6eaefd2d0d
Damon Ross <damonphilipross@gmail.com>
2019-02-26 12:50:22 +1100

    added in validations for all models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,13 @@
 class Challenge < ApplicationRecord
   belongs_to :user
 
+  validates :title, presence: true
   validates :detail, presence: true
   validates :start_date, presence: true
   validates :duration, presence: true
   validates :metric_verb, presence: true
+  validates :metric_value, presence: true
+  validates :metric_frequency, presence: true
+  validates :metric_operator, presence: true
+  validates :location, presence: true
 end
