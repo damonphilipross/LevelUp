893244240e88e32596b3194a91de0cacc64ed36b
dale hindle <dale.hindle@yahoo.ca>
2019-02-27 16:40:25 +1100

    changed the production per alex

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  # this is the thing that alex told me to change but could be wrong WARNING WARNING WARNING
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
